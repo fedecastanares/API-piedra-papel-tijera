e8bb68579205c47f50eed2572e53660b96750197
Federico Castañares <federico.castanares@gmail.com>
2020-10-28 21:43:20 -0300

    get games

--- controllers/games/getGames.js
+++ controllers/games/getGames.js
@@ -4,7 +4,7 @@ module.exports = async (request, response) => {
     try {
         const games = await gamesModel.find({"idHost": request.user.id});
         const pendingGames = await gamesModel.find({"idRival": request.user.id}, "-hostPlay");
-        response.json({mygames: games, pendingGames: pendingGames});
+        response.json({mygames: games, pendingGames: pendingGames, playerId: request.user.id});
     } catch (error) {
         console.log(error);
     }
