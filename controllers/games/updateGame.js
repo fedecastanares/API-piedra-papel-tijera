9c0dd489372c75c00fe4121d1e2b1703edf9606e
Federico Castañares <federico.castanares@gmail.com>
2020-10-31 18:33:09 -0300

    check game is completed

--- controllers/games/updateGame.js
+++ controllers/games/updateGame.js
@@ -19,28 +19,34 @@ module.exports = async(request, response) => {
     const gameCheck = await gamesModel.findById(request.params.id)
     
 
-    if (!validationResult.error && gameCheck) {
+    if (!validationResult.error) {
 
-        const idWin = whoWin(gameCheck.hostPlay, request.body.rivalPlay, gameCheck.idHost, gameCheck.idRival);
+        if (gameCheck.status === 'completed') {
+            response.status(400).json({
+                message: "El juego ya esta completado"
+            })
+        } else {
+            const idWin = whoWin(gameCheck.hostPlay, request.body.rivalPlay, gameCheck.idHost, gameCheck.idRival);
 
-        gamesModel.findOneAndUpdate({_id : request.params.id},{
-            status: request.body.status,
-            rivalPlay: request.body.rivalPlay,
-            idWin: idWin
-        }, 
-        { new: true }, 
-        (error, game) => {
-            if (error) {
-                response.status(500).json({
-                    message: 'No se pudo crear game',
-                    error: error
-                })
-            } else {
-                response.json({
-                   game
-                })
-            }
-        })
+            gamesModel.findOneAndUpdate({_id : request.params.id},{
+                status: request.body.status,
+                rivalPlay: request.body.rivalPlay,
+                idWin: idWin
+            }, 
+            { new: true }, 
+            (error, game) => {
+                if (error) {
+                    response.status(500).json({
+                        message: 'No se pudo crear game',
+                        error: error
+                    })
+                } else {
+                    response.json({
+                    game
+                    })
+                }
+            })
+        }
     } else {
         response.status(400).json({
             message: validationResult.error
