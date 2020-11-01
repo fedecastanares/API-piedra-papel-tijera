79cf7c30326b927ad4d27da89c788ee8686e9ac8
Federico Castañares <federico.castanares@gmail.com>
2020-11-01 20:06:05 -0300

    login

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -2,49 +2,35 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const userModel = require('../../models/user')
 
-module.exports = (request, response) => {
-    try {
-        userModel.findOne({ 'email': request.body.email.toLowerCase() }, (error, user) => {
-            if (error) {
-                response.status(500).json({
-                    message: 'Error al intentar iniciar sesion'
-                })
-            } else {
-               try {
-                    if (user) {
-                        const match = bcrypt.compareSync(request.body.password, user.password)
+module.exports = async (request, response) => {
+
+    const { email, password } = request.body;
+
+     try {
+         let user = await userModel.findOne({ email });
+         if(!user) {
+             return response.status(400).json({msg: 'El usuario no existe'});
+         }
+ 
+         const passOk = bcrypt.compareSync(password, user.password)
+         if(!passOk) {
+             return response.status(400).json({msg: 'Password Incorrecto' })
+         }
+ 
+         const userWithoutPassword = user.toObject()
+         delete userWithoutPassword.password
+         userWithoutPassword.token = jwt.sign({
+            id: user._id,
+            role: user.role
+            }, process.env.JWT_KEY, { expiresIn: '2h' })
         
-                        if (match) {
-                            const userWithoutPassword = user.toObject()
-                            delete userWithoutPassword.password
-                            userWithoutPassword.token = jwt.sign({
-                                id: user._id,
-                                role: user.role
-                            }, process.env.JWT_KEY, { expiresIn: '2h' })
-        
-                            response.json({
-                                user: userWithoutPassword
-                            })
-                        } else {
-                            response.status(401).end().json({
-                                message : response.status(401).end(),
-                                body: request.body
-                            })
-                        }
-                    } else {
-                        response.status(401).end().json({
-                            message : response.status(401).end(),
-                            body: request.body
-                        })
-                    }
-               } catch (error) {
-                response.status(401).end().json({
-                    message : 'password incorrecto'
-                })
-               }
-            }
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+         response.json({
+            user: userWithoutPassword
+         })
+         }
+        catch (error) {
+            response.json({
+                messange: "El usuario no existe"
+             })
+        }
+ }
